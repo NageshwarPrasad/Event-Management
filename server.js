6463c58974346ad23692d7a65cff95956c67f095
git init <132121822+NageshwarPrasad@users.noreply.github.com>
2025-08-17 11:36:21 +0530

    Edit the position of server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const Contact = require("./models/contact");
+const Contact = require("./vikalp-Backend/models/contact");
 
 const app = express();
 app.use(cors());
